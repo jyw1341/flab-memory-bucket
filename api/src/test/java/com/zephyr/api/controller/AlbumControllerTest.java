ca604ff9633a470a47766aba03287ffca587aee0
정용우 <jyw1341@gmail.com>
2024-05-09 00:21:36 +0900

    test: 앨범 컨트롤러 테스트 추가

--- api/src/test/java/com/zephyr/api/controller/AlbumControllerTest.java
+++ api/src/test/java/com/zephyr/api/controller/AlbumControllerTest.java
@@ -2,8 +2,6 @@ package com.zephyr.api.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.zephyr.api.request.AlbumCreate;
-import com.zephyr.api.response.AlbumResponse;
-import org.hamcrest.Matchers;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,15 +9,12 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.stream.LongStream;
+import java.util.Map;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(AlbumController.class)
 class AlbumControllerTest {
@@ -27,69 +22,137 @@ class AlbumControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @Test
-    @DisplayName("앨범 1개를 조회한다")
-    void testGetAlbum() throws Exception {
+    @DisplayName("유효한 앨범 ID를 사용하여 앨범 단건 요청을 하면 200 상태 코드를 반환한다")
+    void givenValidAlbumId_whenGetAlbum_thenStatus200() throws Exception {
         Long albumId = 1L;
-        String albumName = "테스트 앨범";
-        String albumDescription = "hello";
 
         mockMvc.perform(get("/albums/{albumId}", albumId))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.albumId").value(albumId))
-                .andExpect(jsonPath("$.albumName").value(albumName))
-                .andExpect(jsonPath("$.albumDescription").value(albumDescription))
-                .andExpect(jsonPath("$.createdAt").isNotEmpty())
                 .andDo(print());
     }
 
     @Test
-    @DisplayName("앨범 리스트를 조회한다")
-    void testGetAlbumList() throws Exception {
-        List<AlbumResponse> result = LongStream.range(1, 11).mapToObj(value -> AlbumResponse.builder()
-                .albumId(value)
-                .albumName("테스트 앨범" + value)
-                .albumDescription("hello " + value)
-                .createdAt(LocalDateTime.now())
-                .build()
-        ).toList();
+    @DisplayName("유효한 앨범 ID를 사용하여 앨범 단건 요청을 하면 필수 필드만 반환된다")
+    public void givenValidAlbumId_whenGetAlbum_thenRequiredFieldsExist() throws Exception {
+        Long validAlbumId = 1L;
+        String responseJson = mockMvc.perform(get("/albums/{albumId}", validAlbumId))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.albumId").exists())
+                .andExpect(jsonPath("$.albumName").exists())
+                .andExpect(jsonPath("$.albumDescription").exists())
+                .andExpect(jsonPath("$.albumCover").exists())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
 
+        Map<String, Object> responseMap = objectMapper.readValue(responseJson, Map.class);
 
+        assertFalse(responseMap.containsKey("unexpectedField"));
+    }
+
+    @Test
+    @DisplayName("존재하지 않는 앨범 ID를 사용하여 앨범 단건 요청을 하면 404 상태코드를 반환한다")
+    void givenInvalidAlbumId_whenGetAlbum_thenStatus404() throws Exception {
+        mockMvc.perform(get("/posts/{postId}", 1L))
+                .andExpect(status().isNotFound())
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("유효한 앨범 ID를 사용하여 앨범 목록을 요청 하면 200상태코드와 앨범 목록이 반환된다")
+    public void givenValidRequest_whenGetList_thenStatus200AndCorrectAlbums() throws Exception {
         mockMvc.perform(get("/albums"))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.length()", Matchers.is(10)))
+                .andExpect(jsonPath("$").isArray())
+                .andExpect(jsonPath("$").isNotEmpty())
+                .andExpect(jsonPath("$[0].albumId").value(1))
+                .andExpect(jsonPath("$[0].albumName").value("테스트 앨범1"))
+                .andExpect(jsonPath("$[0].albumDescription").value("hello1"))
+                .andExpect(jsonPath("$[0].albumCover").value("url1"))
                 .andDo(print());
     }
 
     @Test
-    @DisplayName("앨범 생성")
-    void testCreateAlbum() throws Exception {
+    @DisplayName("유효한 앨범 생성 요청을 보내고, 201 상태코드와 응답이 정상적으로 세팅되었는지 확인")
+    void givenValidRequest_whenCreateAlbum_thenStatus201AndCorrectResponse() throws Exception {
         AlbumCreate request = AlbumCreate.builder()
                 .ownerId("owner")
                 .albumName("테스트 앨범")
                 .albumDescription("hello")
                 .build();
 
-        Long albumId = 1L;
-        String albumName = "테스트 앨범";
-        String albumDescription = "hello";
-        LocalDateTime dateTime = LocalDateTime.now();
-
-        AlbumResponse response = AlbumResponse.builder()
-                .albumId(albumId)
-                .albumName(albumName)
-                .albumDescription(albumDescription)
-                .createdAt(dateTime)
-                .build();
-
-        ObjectMapper objectMapper = new ObjectMapper();
         String json = objectMapper.writeValueAsString(request);
-
 
         mockMvc.perform(post("/albums")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(json))
                 .andExpect(status().isCreated())
+                .andExpect(header().exists("Location"))
+                .andExpect(jsonPath("$.albumId").exists())
+                .andExpect(jsonPath("$.albumName").value(request.getAlbumName()))
+                .andExpect(jsonPath("$.albumDescription").value(request.getAlbumDescription()))
                 .andDo(print());
+    }
+
+    @Test
+    @DisplayName("필수 필드만 추가한 요청을 보내고, 201 상태코드와 보내지 않은 필드가 비어있는지 확인")
+    void givenValidRequest_whenCreateAlbum_thenStatus201AndOptionalFieldIsNull() throws Exception {
+        AlbumCreate request = AlbumCreate.builder()
+                .ownerId("owner")
+                .albumName("테스트 앨범")
+                .build();
+
+        String json = objectMapper.writeValueAsString(request);
+
+        mockMvc.perform(post("/albums")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json))
+                .andExpect(status().isCreated())
+                .andExpect(header().exists("Location"))
+                .andExpect(jsonPath("$.albumId").exists())
+                .andExpect(jsonPath("$.albumName").value(request.getAlbumName()))
+                .andExpect(jsonPath("$.albumDescription").doesNotExist())
+                .andExpect(jsonPath("$.albumCover").doesNotExist())
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("필수 필드인 앨범 이름이 빠졌을 때, 400 상태코드 확인")
+    public void givenMissingAlbumName_whenCreateAlbum_thenBadRequest() throws Exception {
+        AlbumCreate request = AlbumCreate.builder()
+                .ownerId("owner")
+                .albumDescription("hello")
+                .build();
+
+        String json = objectMapper.writeValueAsString(request);
+
+        mockMvc.perform(post("/albums")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @DisplayName("유효한 앨범 ID를 사용한 삭제 요청을 보내고, 상태코드 204, 앨범이 성공적으로 삭제되는지 확인")
+    public void givenValidAlbumId_whenDeleteAlbum_thenAlbumIsDeleted() throws Exception {
+        Long validAlbumId = 1L;
+
+        mockMvc.perform(delete("/albums/{albumId}", validAlbumId))
+                .andExpect(status().isNoContent());
+
+        //TODO: 삭제 후 해당 앨범이 더 이상 존재하지 않는지 테스트
+    }
+
+    @Test
+    @DisplayName("유효하지 않은 앨범 ID를 사용한 삭제 요청을 보내고, 상태코드 404 확인")
+    public void givenInvalidAlbumId_whenDeleteAlbum_thenNotFound() throws Exception {
+        Long invalidAlbumId = 999L;
+
+        mockMvc.perform(delete("/albums/{albumId}", invalidAlbumId))
+                .andExpect(status().isNotFound());
     }
 }
