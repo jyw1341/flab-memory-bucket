37d59fd2d76fd60cfc43f6430e9606773a7063ad
정용우 <jyw1341@gmail.com>
2024-05-22 10:47:38 +0900

    test: pre-signed url 발급, 파일 업로드 테스트 추가

---
+++ api/src/test/java/com/zephyr/api/service/FileServiceTest.java
@@ -0,0 +1,76 @@
+package com.zephyr.api.service;
+
+import com.zephyr.api.request.FileCreate;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class FileServiceTest {
+
+    @Autowired
+    FileService imageService;
+
+    @Value("${custom.s3.bucket-name}")
+    private String bucketName;
+
+    @Value("${custom.s3.end-point}")
+    private String endPoint;
+
+    @Test
+    @DisplayName("Presigned URL을 사용하여 파일을 업로드/ 업로드한 파일 조회/ 200 반환")
+    public void shouldCreateAndUploadFileSuccessfully() throws Exception {
+        //given
+        Long testId = 1L;
+        FileCreate fileCreate = new FileCreate(UUID.randomUUID().toString(), "jpg");
+        String url = imageService.createPresignedUrl(testId, fileCreate);
+        Map<String, String> metadata = new HashMap<>();
+        metadata.put("x-amz-acl", "public-read");
+
+        HttpResponse<Void> uploadResponse = uploadFile(url, metadata, "sample text");
+
+        // when
+        HttpResponse<Void> headResponse = sendHeadRequest(testId, fileCreate);
+
+        // then
+        assertEquals(HttpURLConnection.HTTP_OK, headResponse.statusCode());
+    }
+
+    private HttpResponse<Void> uploadFile(String url, Map<String, String> metadata, String content) throws Exception {
+        HttpRequest.Builder requestBuilder = HttpRequest.newBuilder();
+        metadata.forEach(requestBuilder::header);
+        HttpClient httpClient = HttpClient.newHttpClient();
+
+        HttpRequest request = requestBuilder
+                .uri(new URL(url).toURI())
+                .PUT(HttpRequest.BodyPublishers.ofString(content))
+                .build();
+
+        return httpClient.send(request, HttpResponse.BodyHandlers.discarding());
+    }
+
+    private HttpResponse<Void> sendHeadRequest(Long testId, FileCreate fileCreate) throws Exception {
+        String url = endPoint + "/" + bucketName + "/" + fileCreate.createKeyName(String.valueOf(testId));
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(new URL(url).toURI())
+                .method("HEAD", HttpRequest.BodyPublishers.noBody())
+                .build();
+
+        HttpClient httpClient = HttpClient.newHttpClient();
+        return httpClient.send(request, HttpResponse.BodyHandlers.discarding());
+    }
+}
