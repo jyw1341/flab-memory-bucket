0e42f222c2b7cc44f40b7e75bba4209456f01dcd
정용우 <jyw1341@gmail.com>
2024-05-22 10:47:09 +0900

    feat: 이미지 업로드를 위한 pre-signed url 발급 기능 추가

---
+++ api/src/main/java/com/zephyr/api/service/FileService.java
@@ -0,0 +1,40 @@
+package com.zephyr.api.service;
+
+import com.zephyr.api.request.FileCreate;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import software.amazon.awssdk.services.s3.presigner.model.PresignedPutObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.model.PutObjectPresignRequest;
+
+import java.time.Duration;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class FileService {
+
+    private final S3Presigner s3Presigner;
+
+    @Value("${custom.s3.bucket-name}")
+    private String bucketName;
+
+    public String createPresignedUrl(Long userId, FileCreate fileCreate) {
+        PutObjectRequest objectRequest = PutObjectRequest.builder()
+                .bucket(bucketName)
+                .key(fileCreate.createKeyName(String.valueOf(userId)))
+                .build();
+
+        PutObjectPresignRequest presignRequest = PutObjectPresignRequest.builder()
+                .signatureDuration(Duration.ofMinutes(10))  // The URL expires in 10 minutes.
+                .putObjectRequest(objectRequest)
+                .build();
+
+        PresignedPutObjectRequest presignedRequest = s3Presigner.presignPutObject(presignRequest);
+
+        return presignedRequest.url().toExternalForm();
+    }
+}
