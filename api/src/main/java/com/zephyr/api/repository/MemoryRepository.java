99a51670771fb8d8d3d8315e5881e28087124c38
정용우 <jyw1341@gmail.com>
2024-05-29 23:53:19 +0900

    feat: 메모리 레포 추가

---
+++ api/src/main/java/com/zephyr/api/repository/MemoryRepository.java
@@ -0,0 +1,7 @@
+package com.zephyr.api.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MemoryRepository {
+}
--- api/src/main/java/com/zephyr/api/service/MemoryService.java
+++ api/src/main/java/com/zephyr/api/service/MemoryService.java
@@ -1,6 +1,7 @@
 package com.zephyr.api.service;
 
 import com.zephyr.api.domain.Memory;
+import com.zephyr.api.repository.MemoryRepository;
 import com.zephyr.api.request.MemoryCreate;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -8,6 +9,8 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class MemoryService {
+
+    private final MemoryRepository memoryRepository;
 
     public Memory create(MemoryCreate memoryCreate) {
 
