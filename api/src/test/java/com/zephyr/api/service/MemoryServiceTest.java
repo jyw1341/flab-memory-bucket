4fa7330701e3ba15ac55a96da375c114e3f058d0
정용우 <jyw1341@gmail.com>
2024-05-30 16:32:01 +0900

    test: 앨범 아이디가 없는 경우 메모리 생성 실패 케이스 추가

--- api/src/test/java/com/zephyr/api/service/MemoryServiceTest.java
+++ api/src/test/java/com/zephyr/api/service/MemoryServiceTest.java
@@ -1,7 +1,9 @@
 package com.zephyr.api.service;
 
 import com.zephyr.api.domain.Album;
+import com.zephyr.api.domain.Member;
 import com.zephyr.api.domain.Memory;
+import com.zephyr.api.exception.AlbumNotFoundException;
 import com.zephyr.api.repository.AlbumRepository;
 import com.zephyr.api.repository.ContentRepository;
 import com.zephyr.api.repository.MemoryRepository;
@@ -16,13 +18,15 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.context.MessageSource;
 
+import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class MemoryServiceTest {
@@ -41,15 +45,18 @@ class MemoryServiceTest {
     private MemoryService memoryService;
 
     @Test
-    @DisplayName("")
-    void test() {
+    @DisplayName("조건에 맞는 요청 / 기억 생성 / 기억 반환")
+    void givenValidRequest_whenCreateMemory_success() {
         //given
         List<ContentCreate> contentCreates = new ArrayList<>();
         contentCreates.add(new ContentCreate("메모 제목1", "메모1", "url1", 1));
         contentCreates.add(new ContentCreate("메모 제목2", "메모2", "url2", 2));
 
+        Member member = Member.builder().id(1L).build();
+        Album album = Album.builder().id(1L).owner(member).build();
+
         MemoryCreate memoryCreate = new MemoryCreate(
-                1L,
+                album.getId(),
                 "제목",
                 "설명",
                 "2024-05-30",
@@ -57,19 +64,59 @@ class MemoryServiceTest {
                 contentCreates
         );
 
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        LocalDate localDate = LocalDate.parse(memoryCreate.getMemoryDate(), dateFormatter);
+
+        Memory memory = Memory.builder()
+                .album(album)
+                .author(album.getOwner())
+                .title(memoryCreate.getTitle())
+                .description(memoryCreate.getDescription())
+                .memoryDate(localDate.atStartOfDay())
+                .build();
+        when(albumRepository.findById(memoryCreate.getAlbumId()))
+                .thenReturn(Optional.of(album));
+        when(memoryRepository.save(any(Memory.class))).thenReturn(memory);
 
         //when
-        when(albumRepository.findById(memoryCreate.getAlbumId()))
-                .thenReturn(Optional.of(Album.builder().build()));
-        Memory memory = memoryService.create(memoryCreate, 1L);
-        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        Memory result = memoryService.create(memoryCreate, 1L);
 
         //then
-        assertEquals(memoryCreate.getAlbumId(), memory.getAlbum().getId());
-        assertEquals(memoryCreate.getTitle(), memory.getTitle());
-        assertEquals(memoryCreate.getDescription(), memory.getDescription());
-        assertEquals(memoryCreate.getMemoryDate(), memory.getMemoryDate().format(dateFormatter));
+        assertNotNull(result);
+        assertEquals(memoryCreate.getAlbumId(), result.getAlbum().getId());
+        assertEquals(memoryCreate.getTitle(), result.getTitle());
+        assertEquals(memoryCreate.getDescription(), result.getDescription());
+        assertEquals(memoryCreate.getMemoryDate(), result.getMemoryDate().format(dateFormatter));
+
+        verify(memoryRepository, times(1)).save(any(Memory.class));
+        verify(contentRepository, times(1)).saveAll(anyList());
     }
 
+    @Test
+    @DisplayName("앨범이 존재 하지 않는 경우 / 기억 생성 / 기억 반환")
+    void givenInvalidAlbumId_whenCreateMemory_returnAlbumNotFound() {
+        //given
+        List<ContentCreate> contentCreates = new ArrayList<>();
+        contentCreates.add(new ContentCreate("메모 제목1", "메모1", "url1", 1));
+        contentCreates.add(new ContentCreate("메모 제목2", "메모2", "url2", 2));
+
+        Member member = Member.builder().id(1L).build();
+        Album album = Album.builder().id(1L).owner(member).build();
+
+        MemoryCreate memoryCreate = new MemoryCreate(
+                album.getId(),
+                "제목",
+                "설명",
+                "2024-05-30",
+                new ArrayList<>(),
+                contentCreates
+        );
+
+        when(albumRepository.findById(memoryCreate.getAlbumId()))
+                .thenReturn(Optional.empty());
+
+        //when then
+        assertThrows(AlbumNotFoundException.class, () -> memoryService.create(memoryCreate, 1L));
+    }
 
 }
