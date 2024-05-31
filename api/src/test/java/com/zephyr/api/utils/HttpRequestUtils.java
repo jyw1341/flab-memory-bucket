90032c06ace54a0c712b92c8ca4d5fa3a9e3d2c8
정용우 <jyw1341@gmail.com>
2024-05-31 18:21:47 +0900

    refactor: 통합 테스트 할 때 url 만들어주는 함수 수정

--- api/src/test/java/com/zephyr/api/utils/HttpRequestUtils.java
+++ api/src/test/java/com/zephyr/api/utils/HttpRequestUtils.java
@@ -2,9 +2,7 @@ package com.zephyr.api.utils;
 
 public class HttpRequestUtils {
 
-    public static final String BASE_URL = "http://localhost:";
-
     public static String createUrl(int port, String path) {
-        return BASE_URL + port + path;
+        return String.format("http://localhost:%d/%s", port, path);
     }
 }
