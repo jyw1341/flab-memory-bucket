bfd17a4ef276a234acbba4b269201caa3fa4a019
정용우 <jyw1341@gmail.com>
2024-06-05 02:07:11 +0900

    feat: 메모리 생성 dto 추가

--- api/src/main/java/com/zephyr/api/request/MemoryCreate.java
+++ api/src/main/java/com/zephyr/api/request/MemoryCreate.java
@@ -5,4 +5,8 @@ import lombok.Data;
 @Data
 public class MemoryCreate {
 
+    private final String caption;
+    private final String contentUrl;
+    private final String locationName;
+    private final String locationUrl;
 }
