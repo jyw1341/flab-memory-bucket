1f42f087a9afb5b0adbff4b510983750ba4d2dc1
정용우 <jyw1341@gmail.com>
2024-06-05 00:54:44 +0900

    feat: 포스트 엔티티 추가

---
+++ api/src/main/java/com/zephyr/api/domain/Post.java
@@ -0,0 +1,59 @@
+package com.zephyr.api.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class Post {
+
+    private Long id;
+    private Album album;
+    private Series series;
+    private Member author;
+    private String title;
+    private String description;
+    private LocalDateTime memoryDate;
+    private String thumbnailUrl;
+    private LocalDateTime createdAt;
+
+    @Builder
+    private Post(Album album, Series series, Member author, String title, String description, LocalDateTime memoryDate, String thumbnailUrl) {
+        this.album = album;
+        this.series = series;
+        this.author = author;
+        this.title = title;
+        this.description = description;
+        this.memoryDate = memoryDate;
+        this.thumbnailUrl = thumbnailUrl;
+    }
+
+    public void setAlbum(Album album) {
+        this.album = album;
+    }
+
+    public void setSeries(Series series) {
+        this.series = series;
+    }
+
+    public void setAuthor(Member author) {
+        this.author = author;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setMemoryDate(LocalDateTime memoryDate) {
+        this.memoryDate = memoryDate;
+    }
+
+    public void setThumbnailUrl(String thumbnailUrl) {
+        this.thumbnailUrl = thumbnailUrl;
+    }
+}
