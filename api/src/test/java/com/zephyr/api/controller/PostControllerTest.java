8c6f3c4d3301ce771fc233a9af5c96da0026e363
정용우 <jyw1341@gmail.com>
2024-06-06 00:09:06 +0900

    fix: 포스트 컨트롤러 테스트 수정

--- api/src/test/java/com/zephyr/api/controller/PostControllerTest.java
+++ api/src/test/java/com/zephyr/api/controller/PostControllerTest.java
@@ -17,7 +17,6 @@ import java.util.List;
 
 import static com.zephyr.api.utils.HttpRequestUtils.createUrl;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class PostControllerTest {
@@ -32,10 +31,10 @@ class PostControllerTest {
     @DisplayName("포스트 생성 요청 성공")
     public void successPostCreateRequest() {
         //given
-        MemoryCreate memoryCreate1 = new MemoryCreate("Caption 1", "Content URL 1", "Location Name 1", "Location URL 1");
-        MemoryCreate memoryCreate2 = new MemoryCreate("Caption 2", "Content URL 2", "Location Name 2", "Location URL 2");
+        MemoryCreate memoryCreate1 = new MemoryCreate(0, "Content URL 1", "Caption 1");
+        MemoryCreate memoryCreate2 = new MemoryCreate(1, "Content URL 2", "Caption 2");
         List<MemoryCreate> memoryCreateList = Arrays.asList(memoryCreate1, memoryCreate2);
-        PostCreate postCreate = new PostCreate(1L, 1L, "Test Title", "Test Description", LocalDate.now(), "Thumbnail URL", memoryCreateList);
+        PostCreate postCreate = new PostCreate(1L, 1L, "Test Title", "Test Description", LocalDate.now(), 0, memoryCreateList);
 
         //when
         ResponseEntity<Void> response = restTemplate.postForEntity(
@@ -46,6 +45,5 @@ class PostControllerTest {
 
         //then
         assertEquals(HttpStatus.CREATED, response.getStatusCode());
-        assertNotNull(response.getHeaders().getLocation().getPath());
     }
 }
