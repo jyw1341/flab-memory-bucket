e2f7863a3d49a80b7427927b80041380bed827fc
정용우 <jyw1341@gmail.com>
2024-06-19 22:28:19 +0900

    test: 테스트 후 테이블 초기화 기능 추가

--- api/src/test/java/com/zephyr/api/utils/TestRequestUtils.java
+++
@@ -1,8 +0,0 @@
-package com.zephyr.api.utils;
-
-public class TestRequestUtils {
-
-    public static String createUrl(int port, String path) {
-        return String.format("http://localhost:%d/%s", port, path);
-    }
-}
