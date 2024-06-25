436ccb427a9a32d1e8ee45636b71820fdaa8c98e
정용우 <jyw1341@gmail.com>
2024-06-25 18:03:37 +0900

    feat: 앨범 목록 조회 요청 DTO 추가

--- api/src/main/java/com/zephyr/api/dto/service/AlbumListServiceDto.java
+++ api/src/main/java/com/zephyr/api/dto/service/AlbumListServiceDto.java
@@ -1,13 +1,17 @@
 package com.zephyr.api.dto.service;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
 public class AlbumListServiceDto {
 
     private final Long memberId;
     private final Integer page;
     private final Integer size;
+
+    public AlbumListServiceDto(Long memberId, Integer page, Integer size) {
+        this.memberId = memberId;
+        this.page = (page != null) ? page : 1;
+        this.size = (size != null) ? size : 10;
+    }
 }
